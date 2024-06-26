a48c708eac0aeaac58b66ba0493a4b40287fb1da
NRR <pianzovmaxsim236@gmail.com>
2024-06-26 17:40:48 +0300

    fix

--- BOMJ/Models/SpravkaContext.cs
+++ BOMJ/Models/SpravkaContext.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-
+// Использование и применение модели данных и context 
 namespace BOMJ.Models;
 
 public partial class SpravkaContext : DbContext
