b44d21d1994be72d439c3d01ca454588f87dca6c
NRR <pianzovmaxsim236@gmail.com>
2024-06-26 17:41:33 +0300

    fix all

--- BOMJ/Models/SpravkaContext.cs
+++ BOMJ/Models/SpravkaContext.cs
@@ -22,7 +22,7 @@ public partial class SpravkaContext : DbContext
     public virtual DbSet<Sotrud> Sotruds { get; set; }
 
     public virtual DbSet<User> Users { get; set; }
-
+    //Строка подключения
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Spravka;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
